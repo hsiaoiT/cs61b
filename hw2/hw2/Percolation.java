d3d0bad2b54a1e7338f957bc14bbe7e23acb0101
Tung <hsiaotung@gmail.com>
2018-07-31 18:18:28 +0800

    Haven't test yet!

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,176 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    int N;
+    boolean sites[][];
+    int numberOfOpenSites;
+    WeightedQuickUnionUF nthSitesFromTop;
+    WeightedQuickUnionUF nthSites;
+    WeightedQuickUnionUF nthSitesFromBottom;
+
+    /** Creates N-by-N grid, with all sites initially blocked. */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        sites = new boolean[N * N][2];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < 2; j += 1) {
+                sites[i][j] = false;
+            }
+        }
+
+        numberOfOpenSites = 0;
+
+        nthSitesFromTop = new WeightedQuickUnionUF(N * N);
+        for (int i = 0; i < N - 1; i += 1) {
+            nthSitesFromTop.union(i, i + 1);
+        }
+
+        //nthSites = new WeightedQuickUnionUF(N * N);
+
+        nthSitesFromBottom = new WeightedQuickUnionUF(N * N);
+        for (int i = 0; i < N - 1; i += 1) {
+            nthSitesFromTop.union(i, i + 1);
+        }
+        for (int i = N * N - N; i < N * N - N - 1; i += 1) {
+            nthSitesFromBottom.union(i, i + 1);
+        }
+    }
+
+    /** Returns the number of site given the row and column. */
+    private int xyTo1D(int r, int c) {
+        return r * N + c;
+    }
+
+    /** open the site (row, col) if it is not open already. */
+    public void open(int row, int col) {
+        if (row > N-1 || col > N-1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (N == 1) {
+            sites[0][0] = true;
+            sites[0][1] = true;
+            return;
+        }
+        int ithSite = xyTo1D(row, col);
+        if (!isOpen(row, col)) {
+            sites[ithSite][0] = true;
+            numberOfOpenSites += 1;
+        }
+        int[] neighbors = findNeighbors(ithSite);
+        if (ithSite < N) {
+            sites[ithSite][1] = true;
+        }
+
+        for(int neighbor : neighbors) {
+            if(isOpen(neighbor)) {
+                //nthSites.union(ithSite, neighbor);
+                nthSitesFromTop.union(ithSite, neighbor);
+                nthSitesFromBottom.union(ithSite, neighbor);
+            }
+        }
+
+    }
+
+    /** If the site (row, col) opens. */
+    public boolean isOpen(int row, int col) {
+        if (row > N-1 || col > N-1) {
+            throw new IndexOutOfBoundsException();
+        }
+        int ithSite = xyTo1D(row, col);
+        return sites[ithSite][0];
+    }
+    private boolean isOpen(int ithSite) {
+        return sites[ithSite][0];
+    }
+
+    /** Finds neightbors of ithSite as N > 1. */
+    private int[] findNeighbors(int ithSite) {
+        int[] neighbors;
+        //Check the 0 site.
+        if (ithSite < 1) {
+            neighbors = new int[2];
+            neighbors[0] = 1;
+            neighbors[1] = ithSite+N;
+        }
+        // Check the 0th row sites except N-1;
+        else if (ithSite < N - 1) {
+            neighbors = new int[3];
+            neighbors[0] = ithSite-1;
+            neighbors[1] = ithSite+1;
+            neighbors[2] = ithSite+N;
+        }
+        // Check (0, N-1);
+        else if (ithSite == N -1) {
+            neighbors = new int[2];
+            neighbors[0] = ithSite-1;
+            neighbors[1] = ithSite+N;
+        }
+        // Check (N-1, 0);
+        else if (ithSite == N * (N-1)) {
+            neighbors = new int[2];
+            neighbors[0] = ithSite-N;
+            neighbors[1] = ithSite+1;
+        }
+        // Check the N-1 th row sites except N^2-1
+        else if (ithSite > N*N - N - 1 && ithSite < N*N-1) {
+            neighbors = new int[3];
+            neighbors[0] = ithSite-N;
+            neighbors[1] = ithSite-1;
+            neighbors[2] = ithSite+1;
+        }
+        // Check (N-1, N-1)
+        else if (ithSite == N*N-1) {
+            neighbors = new int[2];
+            neighbors[0] = ithSite-N;
+            neighbors[1] = ithSite-1;
+        }
+        // Check 0th col
+        else if (ithSite%N == 0) {
+            neighbors = new int[3];
+            neighbors[0] = ithSite-N;
+            neighbors[1] = ithSite+N;
+            neighbors[2] = ithSite+1;
+        }
+        // Check N-1 th col;
+        else if (ithSite%N == N-1) {
+            neighbors = new int[3];
+            neighbors[0] = ithSite-N;
+            neighbors[1] = ithSite+N;
+            neighbors[2] = ithSite-1;
+        } else {
+            neighbors = new int[4];
+            neighbors[0] = ithSite+1;
+            neighbors[1] = ithSite-1;
+            neighbors[2] = ithSite+N;
+            neighbors[3] = ithSite-N;
+        }
+        return neighbors;
+    }
+
+    /** If the site (row, col) full. */
+    public boolean isFull(int row, int col) {
+        if (row > N-1 || col > N-1) {
+            throw new IndexOutOfBoundsException();
+        }
+        int ithSite = xyTo1D(row, col);
+        if (ithSite >= N) {
+            if (nthSitesFromTop.connected(ithSite, 0)) {
+                sites[ithSite][1] = true;
+            }
+        }
+        return sites[ithSite][1];
+    }
+
+    /** Returns the number of open sites. */
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+    /** If the system percolate. */
+    public boolean percolates() {
+        return nthSitesFromBottom.connected(0, N*N-1);
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,71 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private int N;
+    private int T;
+    private double[] percol;
+    private double mean;
+    private double stddev;
+    StdRandom random;
+    /** Performs T independent experiments on an N-by-N grid. */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        this.T = T;
+        percol = new double[T];
+        for (int i = 0; i < T; i += 1) {
+            Percolation percolation = pf.make(N);
+            int threshold = 0;
+            while(!percolation.percolates()) {
+                int site = random.uniform(N*N);
+                int[] pos = oneDToXy(site);
+
+                while (percolation.isOpen(pos[0], pos[1])) {
+                    site = random.uniform(N*N);
+                    pos = oneDToXy(site);
+                }
+                percolation.open(pos[0], pos[1]);
+                threshold += 1;
+            }
+            percol[i] = threshold;
+        }
+
+        mean = StdStats.mean(percol);
+        stddev = StdStats.stddev(percol);
+    }
+    private int[] oneDToXy(int site) {
+        int[] position = new int[2];
+        position[0] = site/N;
+        position[1] = site%N;
+        return position;
+    }
+    /** Sample mean of percolation threshold. */
+    public double mean() {
+
+        return mean;
+    }
+    /** Sample standard deviation of percolation threshold. */
+    public double stddev() {
+        return stddev;
+    }
+
+    private double helpConfidence() {
+        double twoItem = 1.96 * stddev/Math.pow(T, 0.5);
+        return twoItem;
+    }
+    /** Low endpoint of 95% confidence interval. */
+    public double confidenceLow() {
+        double twoItem = helpConfidence();
+        return mean-twoItem;
+    }
+    /** High endpoint of 95% confidence interval. */
+    public double confidenceHigh() {
+        double twoItem = helpConfidence();
+        return mean+twoItem;
+    }
 }
